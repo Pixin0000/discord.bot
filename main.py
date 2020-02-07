d982c7a49082f37a6a7b8119feca73afe6073ea7
Pixin1217 <54139529+Pixin1217@users.noreply.github.com>
2020-02-07 17:04:11 +0800

    有了基本功能+音樂撥放功能

---
+++ main.py
@@ -0,0 +1,92 @@
+import discord
+from discord.ext import commands
+import json
+import random
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
+with open('setting.json', 'r', encoding='utf-8') as jfile:
+    jdata = json.load(jfile)
+bot = commands.Bot(command_prefix='!')
+f = open("bot\\JMusicBot-0.2.6.jar" ,'wb')
+
+@bot.event
+async def on_ready():
+    print("Bot is online!")
+
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(670963161486065677)
+    await channel.send(f'{member}join!')
+
+
+async def on_member_remove(member):
+    channel = bot.get_channel(670963161486065677)
+    await channel.send(f'{member}leave!')
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+
+
+@bot.command()
+async def 圖片(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    await ctx.send(random_pic)
+
+
+@bot.command()
+async def pic(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file=pic)
+
+
+@bot.command()
+async def 臭狗(ctx):
+    await ctx.send('陳奕祥')
+
+@bot.command()
+async def test(ctx):
+    keyboard.press("f5");
+    keyboard.release("f5");
+    await ctx.send('123');
+
+@bot.command()
+async def 龜兒子(ctx):
+    await ctx.send('陳奕祥')
+
+
+@bot.command()
+async def 武漢肺炎(ctx):
+    await ctx.send('去陳奕祥家')
+
+
+@bot.command()
+async def nmsl(ctx):
+    await ctx.send('你媽死了')
+
+
+@bot.command()
+async def 成員(ctx):
+    member = bot.get_all_members()
+    await ctx.send(member)
+
+
+@bot.command()
+async def 陳奕翔(ctx):
+    await ctx.send('快點上線')
+
+
+@bot.command()
+async def 指令(ctx):
+    for data in jdata['construct']:
+        await ctx.send(data.strip())
+@bot.command()
+async def loading(ctx):
+    bot.reload_extension('reloading...');
+    await discord.Disconnect();    
+
+bot.run('NjY5MjM5NjAyNzQ1MzExMjM5.Xi17kw.dwcg-BgThk2gSO8cXnuSYLtwvkI') 
+
